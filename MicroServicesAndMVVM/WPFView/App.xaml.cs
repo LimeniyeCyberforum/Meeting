0d5847e4324b521a09a1f6298b9ea52909f7f98a
limeniye <limeniye@gamanet.com>
2022-05-17 23:45:49 +0300

    - Created WPFView project;

---
+++ MicroServicesAndMVVM/WPFView/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace WPFView
+{
+    public partial class App : Application
+    {
+        private void OnApplicationLaunched(object sender, StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+        }
+    }
+}
