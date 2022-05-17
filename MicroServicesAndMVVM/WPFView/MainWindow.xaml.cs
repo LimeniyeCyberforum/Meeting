845518139646540dc8a50e83e7ffc8281f5bd2df
limeniye <limeniye@gamanet.com>
2022-05-18 00:56:32 +0300

    - Added ObservableCollection for messages;

--- MicroServicesAndMVVM/WPFView/MainWindow.xaml.cs
+++ MicroServicesAndMVVM/WPFView/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Grpc;
 using Grpc.Net.Client;
+using System;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Windows;
@@ -8,22 +10,27 @@ namespace WPFView
 {
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Message> Messages { get; } = new ObservableCollection<Message>()
+        {
+            new Message(Guid.NewGuid(), "Hello world", false, MessageStatus.Readed, DateTime.Now)
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+
+            //var httpHandler = new HttpClientHandler();
+            //httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
 
 
-            var httpHandler = new HttpClientHandler();
-            httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            //using var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions { HttpHandler = httpHandler });
+            //var client = new Greeter.GreeterClient(channel);
+
+            //var replay = client.SayHello(new HelloRequest() { Name = "Hello World!" } );
 
 
-            using var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions { HttpHandler = httpHandler });
-            var client = new Greeter.GreeterClient(channel);
-
-            var replay = client.SayHello(new HelloRequest() { Name = "Hello World!" } );
-
-
-            Debug.WriteLine(replay.Message);
+            //Debug.WriteLine(replay.Message);
         }
     }
 }
