c1faa6844553931a7327442b71358cc2b848dec9
limeniye <limeniye@gamanet.com>
2022-05-18 00:37:40 +0300

    - Added message type;

---
+++ MicroServicesAndMVVM/WPFView/Message.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WPFView
+{
+    public enum MessageStatus
+    {
+        Sending,
+        Unread,
+        Readed
+    }
+
+    public class Message
+    {
+        public Guid Id { get; }
+        public string Text { get; }
+        public bool IsEdited { get; }
+        public MessageStatus Status { get; }
+        public DateTime DateTime { get; }
+
+        public Message(Guid id, string text, bool isEdited, MessageStatus status, DateTime dateTime)
+        {
+            Id = id;
+            Text = text;
+            IsEdited = isEdited;
+            Status = status;
+            DateTime = dateTime;
+        }
+    }
+}
