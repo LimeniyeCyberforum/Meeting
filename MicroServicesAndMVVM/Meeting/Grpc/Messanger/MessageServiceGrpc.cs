09c38a25a6ec8369a39ff761e31b4667f432c7a4
limeniye <limeniye@gamanet.com>
2022-05-18 18:40:37 +0300

    - Added singleton for meeting;

---
+++ MicroServicesAndMVVM/Meeting/Grpc/Messanger/MessageServiceGrpc.cs
@@ -0,0 +1,26 @@
+﻿using Meeting.Abstractions.Interfaces.Messanger;
+using Meeting.Abstractions.Messanger;
+using System;
+using System.Threading.Tasks;
+
+namespace Meeting.Grpc.Messanger
+{
+    internal class MessageServiceGrpc : MessageServiceAbstract, IMessageService
+    {
+        public override void SendMessage(Guid guid, string username, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task SendMessageAsync(Guid guid, string username, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MessageServiceGrpc()
+            :base()
+        {
+
+        }
+    }
+}
---
+++ MicroServicesAndMVVM/Meeting/MeetingService.cs
@@ -0,0 +1,51 @@
+﻿using Meeting.Abstractions.Interfaces.Messanger;
+using Meeting.Grpc.Messanger;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Meeting
+{
+    public enum DataInteractingType
+    {
+        Local,
+        Wcf,
+        Grpc
+    }
+
+    public sealed class MeetingService
+    {
+        private static MeetingService _instance = null;
+
+        public IMessageService MeetingConnector { get; private set; }
+
+        protected MeetingService() { }
+
+        public static MeetingService Instance(DataInteractingType dataInteracting)
+        {
+            if (_instance == null)
+            {
+                _instance = new MeetingService();
+                _instance.Initialize(dataInteracting);
+
+                return _instance;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void Initialize(DataInteractingType dataInteracting)
+        {
+            switch (dataInteracting)
+            {
+                case DataInteractingType.Grpc:
+                    MeetingConnector = new MessageServiceGrpc();
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
