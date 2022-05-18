64517f66f54e780f2905f21401b6f572370939fc
limeniye <limeniye@gamanet.com>
2022-05-18 15:21:33 +0300

    - Added timestamp;

--- MicroServicesAndMVVM/GrpsServer/Services/MessangerService.cs
+++ MicroServicesAndMVVM/GrpsServer/Services/MessangerService.cs
@@ -1,5 +1,6 @@
 using Grpc.Core;
 using GrpsServer;
+using Google.Protobuf.WellKnownTypes;
 
 namespace GrpsServer.Services
 {
@@ -15,20 +16,33 @@ namespace GrpsServer.Services
         {
             return Task.FromResult(new MessageResponse
             {
-                
+                Time = GetCurrentTime(),
+                Username = request.Username,
             });
         }
 
         public override async Task MessageStream(IAsyncStreamReader<MessageRequest> requestStream, 
-            IServerStreamWriter<MessageResponse> responseStream, ServerCallContext context)
+            IServerStreamWriter<MessageReplay> responseStream, ServerCallContext context)
         {
             await foreach (var request in requestStream.ReadAllAsync())
             {
-                await responseStream.WriteAsync(new MessageResponse()
+                await responseStream.WriteAsync(new MessageReplay()
                 {
-                    //Message = "Hello " + request.Name + " " + DateTime.UtcNow
+                    Username = request.Username,
+                    Message = request.Message,
+                    Time = GetCurrentTime()
                 });
             }
+        }
+
+        private Timestamp GetCurrentTime()
+        {
+            Timestamp timestamp = new Timestamp()
+            {
+                Seconds = DateTime.Now.Second,
+                Nanos = DateTime.Now.Second * 100
+            };
+            return timestamp;
         }
     }
 }
