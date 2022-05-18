5a57b1286a956f938b5fec6354e6eed06bcf6deb
limeniye <limeniye@gamanet.com>
2022-05-18 17:27:29 +0300

    - Changed target framework to netstandard 2.0;

--- MicroServicesAndMVVM/Meeting/Abstractions/Interfaces/Messanger/IMessageService.cs
+++ MicroServicesAndMVVM/Meeting/Abstractions/Interfaces/Messanger/IMessageService.cs
@@ -1,7 +1,18 @@
-﻿namespace Meeting.Abstractions.Interfaces.Messanger
+﻿using Common.EventArgs;
+using Meeting.DataTypes.Messanger;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Meeting.Abstractions.Interfaces.Messanger
 {
     public interface IMessageService
     {
-        IReadOnlyDictionary<Guid, >
+        event  EventHandler<NotifyDictionaryChangedEventArgs<Guid, MessageDto>> MessagesChanged;
+
+        IReadOnlyDictionary<Guid, MessageDto> Messages { get; }
+
+        void SendMessage(Guid guid, string username, string message);
+        Task SendMessageAsync(Guid guid, string username, string message);
     }
 }
