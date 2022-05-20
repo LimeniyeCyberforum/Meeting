ef739636811e28bf75797a13e48a97f363fe175e
limeniye <limeniye@gamanet.com>
2022-05-20 03:03:32 +0300

    - Added chatService;

---
+++ MicroServicesAndMVVM/GrpsServer/Model/ChatService.cs
@@ -0,0 +1,39 @@
+﻿using GrpcCommon;
+using System;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace GrpsServer.Model
+{
+    [Export]
+    public class ChatService
+    {
+        private readonly ILogger<ChatService> _logger;
+
+        public ChatService(ILogger<ChatService> logger)
+        {
+            _logger = logger;
+        }
+
+        [Import]
+        private IChatLogRepository repository = null;
+        private event Action<MessageFromLobby> Added;
+
+        public void Add(MessageFromLobby chatLog)
+        {
+            _logger.LogInformation($"{chatLog}");
+
+            repository.Add(chatLog);
+            Added?.Invoke(chatLog);
+        }
+
+        public IObservable<MessageFromLobby> GetChatLogsAsObservable()
+        {
+            var oldLogs = repository.GetAll().ToObservable();
+            var newLogs = Observable.FromEvent<MessageFromLobby>((x) => Added += x, (x) => Added -= x);
+
+            return oldLogs.Concat(newLogs);
+        }
+    }
+}
