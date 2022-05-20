2a7906bc5c0efea6c8ef231e2d060b92deb77b00
limeniye <limeniye@gamanet.com>
2022-05-21 02:27:47 +0300

    - Fixed user random guid;

--- MicroServicesAndMVVM/GrpsServer/Services/MeetingService.cs
+++ MicroServicesAndMVVM/GrpsServer/Services/MeetingService.cs
@@ -21,12 +21,13 @@ namespace GrpsServer.Services
         private UsersCameraCaptureService usersCameraCaptureService = null;
         private readonly Empty empty = new Empty();
 
-        private readonly Dictionary<Guid, string> users = new Dictionary<Guid, string>();
+        private static readonly Dictionary<Guid, string> users = new Dictionary<Guid, string>();
 
 
         public override Task<ConnectResponse> Connect(ConnectRequest request, ServerCallContext context)
         {
-            if (!users.TryAdd(Guid.NewGuid(), request.Username))
+            var newUserGuid = Guid.NewGuid();
+            if (!users.TryAdd(newUserGuid, request.Username))
             {
                 return Task.FromResult(new ConnectResponse
                 {
@@ -38,7 +39,7 @@ namespace GrpsServer.Services
             return Task.FromResult(new ConnectResponse
             {
                 IsSuccessfully = true,
-                Guid = Guid.NewGuid().ToString()
+                Guid = newUserGuid.ToString()
             });
         }
 
