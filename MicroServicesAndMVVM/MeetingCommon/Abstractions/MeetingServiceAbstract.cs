5c54a1ee2fdffb50793eec810e2f81de2e349747
limeniye <limeniye@gamanet.com>
2022-05-20 19:04:35 +0300

    - Added abstract methods to MeetingServiceAbstract;

--- MicroServicesAndMVVM/MeetingCommon/Abstractions/MeetingServiceAbstract.cs
+++ MicroServicesAndMVVM/MeetingCommon/Abstractions/MeetingServiceAbstract.cs
@@ -1,7 +1,18 @@
-﻿namespace MeetingCommon.Abstractions
+﻿using MeetingCommon.Abstractions.CameraCapture;
+using MeetingCommon.Abstractions.Messanger;
+using MeetingCommon.DataTypes;
+using System.Threading.Tasks;
+
+namespace MeetingCommon.Abstractions
 {
     public abstract class MeetingServiceAbstract
     {
+        public abstract MessageServiceAbstract MessageService { get; }
 
+        public abstract CameraCaptureServiceAbstract CameraCaptureService { get; }
+
+        public abstract UserDto Connect(string username);
+
+        public abstract Task<UserDto> ConnectAsync(string username);
     }
 }
