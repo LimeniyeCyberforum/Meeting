24fc2bf40aea4f18e933fdf522d4d5de14eebc01
limeniye <limeniye@gamanet.com>
2022-05-20 22:07:19 +0300

    - MessageService realized;

--- MicroServicesAndMVVM/MeetingGrpcClient/MessageService.cs
+++ MicroServicesAndMVVM/MeetingGrpcClient/MessageService.cs
@@ -1,15 +1,9 @@
 ﻿using Common.EventArgs;
-using Google.Protobuf;
 using Google.Protobuf.WellKnownTypes;
-using Grpc.Core;
-using Grpc.Net.Client;
 using GrpcCommon;
 using MeetingCommon.Abstractions.Messanger;
-using MeetingCommon.DataTypes.Messanger;
 using System;
-using System.IO;
 using System.Linq;
-using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -21,49 +15,10 @@ namespace MeetingGrpcClient
 
         private readonly Meeting.MeetingClient _client;
 
-
-        public MessageService()
+        public MessageService(Meeting.MeetingClient client)
             : base()
         {
-            var secure = false;
-
-            if (secure)
-            {
-                //var httpHandler = new HttpClientHandler();
-
-                // Here you can disable validation for server certificate for your easy local test
-                // See https://docs.microsoft.com/en-us/aspnet/core/grpc/troubleshoot#call-a-grpc-service-with-an-untrustedinvalid-certificate
-                //httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
-
-                //_client = new Meeting.MeetingClient(GrpcChannel.ForAddress("https://localhost:50052", new GrpcChannelOptions { HttpHandler = httpHandler }));
-
-                //    var httpHandler = new HttpClientHandler();
-                //    httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
-
-                //    using var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions { HttpHandler = httpHandler });
-                //    _client = new GrpsServer.Messanger.MessangerClient(channel);
-
-
-                var serverCACert = File.ReadAllText(@"C:\localhost_server.crt");
-                var clientCert = File.ReadAllText(@"C:\localhost_client.crt");
-                var clientKey = File.ReadAllText(@"C:\localhost_clientkey.pem");
-                var keyPair = new KeyCertificatePair(clientCert, clientKey);
-                //var credentials = new SslCredentials(serverCACert, keyPair);
-
-                // Client authentication is an option. You can remove it as follows if you only need SSL.
-                var credentials = new SslCredentials(serverCACert);
-
-                _client = new Meeting.MeetingClient(
-                    new Channel("localhost", 5001, credentials));
-
-
-            }
-            else
-            {
-                // create insecure channel
-                _client = new Meeting.MeetingClient(
-                    new Channel("localhost", 5001, ChannelCredentials.Insecure));
-            }
+            _client = client;
         }
 
         public override Task ChatSubscribeAsync()
@@ -81,6 +36,16 @@ namespace MeetingGrpcClient
             throw new NotImplementedException();
         }
 
+        public override void SendMessage(Guid messageGuid, Guid userGuid, string message)
+        {
+            _client.SendMessage(new MessageRequest()
+            {
+                MessageGuid = messageGuid.ToString(),
+                UserGuid = userGuid.ToString(),
+                Message = message
+            });
+        }
+
         public override async Task SendMessageAsync(Guid messageGuid, Guid userGuid, string message)
         {
             await _client.SendMessageAsync(new MessageRequest() 
@@ -89,25 +54,7 @@ namespace MeetingGrpcClient
                 UserGuid = userGuid.ToString(),
                 Message = message
             });
-
         }
-
-        public override Task SendOwnCameraCaptureAsync(MemoryStream stream)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override Task UsersCameraCaptureSubscribeAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override Task UsersCameraCaptureUnsubscribeAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-
 
         //private AsyncDuplexStreamingCall<MessageRequest, MessageReplay> _call;
         //private AsyncDuplexStreamingCall<CameraCaptureTest, CameraCaptureTest> _call2;
