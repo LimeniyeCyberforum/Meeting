6aadb950fc5cd5e880a7e019684153da87ef2510
limeniye <limeniye@gamanet.com>
2022-05-20 22:54:21 +0300

    - Added IMeetingConnectionService;

--- MicroServicesAndMVVM/MeetingCommon/Abstractions/MeetingServiceAbstract.cs
+++ MicroServicesAndMVVM/MeetingCommon/Abstractions/MeetingServiceAbstract.cs
@@ -1,18 +1,46 @@
 ﻿using MeetingCommon.Abstractions.CameraCapture;
 using MeetingCommon.Abstractions.Messanger;
 using MeetingCommon.DataTypes;
+using System;
 using System.Threading.Tasks;
 
 namespace MeetingCommon.Abstractions
 {
-    public abstract class MeetingServiceAbstract
+    public enum ConnectionAction
     {
-        public abstract MessageServiceAbstract MessageService { get; protected set; }
+        Connected,
+        Disconnected
+    }
 
-        public abstract CameraCaptureServiceAbstract CameraCaptureService { get; protected set; }
+    public interface IMeetingConnectionService
+    {
+        ConnectionAction CurrentConnectionState { get; }
+
+        event EventHandler<ConnectionAction> ConnectionStateChanged;
+
+        UserDto Connect(string username);
+
+        Task<UserDto> ConnectAsync(string username);
+    }
+
+    public abstract class MeetingServiceAbstract : IMeetingConnectionService
+    {
+        public ConnectionAction CurrentConnectionState { get; protected set; }
+
+        public event EventHandler<ConnectionAction> ConnectionStateChanged;
+
+        public MessageServiceAbstract MessageService { get; protected set; }
+
+        public CameraCaptureServiceAbstract CameraCaptureService { get; protected set; }
 
         public abstract UserDto Connect(string username);
 
         public abstract Task<UserDto> ConnectAsync(string username);
+
+        protected void RaiseConnectionStateChangedAction(ConnectionAction action)
+        {
+            CurrentConnectionState = action;
+            ConnectionStateChanged?.Invoke(this, action);
+        }
     }
 }
