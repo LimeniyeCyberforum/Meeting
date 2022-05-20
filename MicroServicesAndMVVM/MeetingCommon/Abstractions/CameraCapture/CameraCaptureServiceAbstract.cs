788a94de8eb4711a540c2ad5b38e4e310d99ec93
limeniye <limeniye@gamanet.com>
2022-05-20 18:49:27 +0300

    - Added CameraCaptureServiceAbstract;

---
+++ MicroServicesAndMVVM/MeetingCommon/Abstractions/CameraCapture/CameraCaptureServiceAbstract.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MeetingCommon.Abstractions.CameraCapture
+{
+    public delegate void CameraFrameChangedHandler(object sender, Guid userGuid, byte[] frameBytes);
+
+    public abstract class CameraCaptureServiceAbstract
+    {
+        public Guid CurrentUserGuid { get; }
+
+        public event CameraFrameChangedHandler CameraFrameChanged;
+
+        public CameraCaptureServiceAbstract(Guid currentUserGuid)
+        {
+            CurrentUserGuid = currentUserGuid;
+        }
+
+        protected void RaiseCameraFrameChangedAction(Guid userGuid, byte[] frameBytes)
+        {
+            CameraFrameChanged?.Invoke(this, userGuid, frameBytes);
+        }
+
+        public abstract Task SendOwnCameraCaptureAsync(MemoryStream stream);
+
+        public abstract Task UsersCameraCaptureSubscribeAsync();
+
+        public abstract Task UsersCameraCaptureUnsubscribeAsync();
+    }
+}
