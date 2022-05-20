928f23834ba2921630c9992da2a216b25a367931
limeniye <limeniye@gamanet.com>
2022-05-20 18:54:19 +0300

    - Added UserDto;

---
+++ MicroServicesAndMVVM/MeetingCommon/DataTypes/UserDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MeetingCommon.DataTypes
+{
+    public class UserDto : IEquatable<UserDto>
+    {
+        public Guid Guid { get; }
+
+        public string UserName { get; }
+
+        private readonly int hash;
+
+        public UserDto(Guid guid, string userName)
+        {
+            Guid = guid;
+            UserName = userName ?? string.Empty;
+
+            hash = (Guid, UserName).GetHashCode();
+        }
+
+        public bool Equals(UserDto other)
+        {
+            return other.Guid == Guid &&
+                Equals(other.UserName, UserName);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is UserDto currency && Equals(currency);
+        }
+
+        public override int GetHashCode() => hash;
+    }
+}
