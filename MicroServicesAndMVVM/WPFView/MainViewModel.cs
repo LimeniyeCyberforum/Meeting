5f99db3dd7ceefb01aa680de95782aa7ee7fea96
limeniye <limeniye@gamanet.com>
2022-05-20 15:27:43 +0300

    - MainViewModel ConnectViewModel property;

---
+++ MicroServicesAndMVVM/WPFView/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MeetingCommon.Abstractions.Messanger;
+using WPFView.Connect;
+
+namespace WPFView
+{
+    public class MainViewModel : BaseInpc
+    {
+        public ConnectViewModel ConnectVM { get; }
+
+        public MainViewModel(MessageServiceAbstract messageService)
+        {
+            ConnectVM = new ConnectViewModel();
+
+        }
+    }
+}
