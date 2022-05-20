a048586990bb2c90c32df23e678cbb0a216f7c39
limeniye <limeniye@gamanet.com>
2022-05-20 14:31:19 +0300

    - Added type axtensions;

---
+++ MicroServicesAndMVVM/MeetingGrpcClient/TypeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using GrpcCommon;
+using MeetingCommon.DataTypes.Messanger;
+using System;
+
+namespace MeetingGrpcClient
+{
+    internal static class TypeExtensions
+    {
+        public static MessageDto ToMessageDto(this MessageFromLobby messageFromLobby)
+        {
+            if (!Guid.TryParse(messageFromLobby.MessageGuid, out Guid messageGuid))
+                throw new ArgumentException($"Lobby MessageGuid {messageFromLobby.MessageGuid} is not Guid.");
+
+            return new MessageDto(messageGuid, messageFromLobby.Message, messageFromLobby.Username, messageFromLobby.Time.ToDateTime());
+        }
+    }
+}
