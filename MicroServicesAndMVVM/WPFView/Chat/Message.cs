9868b7187196f48ef826845b2085d33bdbee4c06
limeniye <limeniye@gamanet.com>
2022-05-21 17:06:19 +0300

    - Added OwnMessage;

---
+++ MicroServicesAndMVVM/WPFView/Chat/Message.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WPFView.Chat
+{
+    public enum MessageStatus
+    {
+        Sending,
+        Unread,
+        Readed
+    }
+
+    public class Message
+    {
+        public Guid Id { get; }
+        public string Text { get; }
+        public DateTime? DateTime { get; }
+
+        public Message(Guid id, string text, DateTime? dateTime)
+        {
+            Id = id;
+            Text = text;
+            DateTime = dateTime;
+        }
+    }
+
+    public class OwnMessage : Message
+    {
+        public MessageStatus Status { get; }
+
+        public OwnMessage(Guid id, string text, MessageStatus status, DateTime? dateTime)
+            : base(id, text, dateTime)
+        {
+            Status = status;
+        }
+    }
+}
