95b8259ee174cd2580ada01c7c7ef0527a4488f8
limeniye <limeniye@gamanet.com>
2022-05-21 17:10:26 +0300

    - Messages DataTemplateselector;

---
+++ MicroServicesAndMVVM/WPFView/Chat/MessageItemDataTemplateSelector.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFView.Chat
+{
+    public class MessageItemDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate OwnerMessage { get; set; }
+        public DataTemplate AnotherMessage { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is null)
+                throw new System.NullReferenceException("Element is null. [SelectTemplate]");
+
+            return SelectTemplateCore(item);
+        }
+
+        private DataTemplate SelectTemplateCore(object item)
+        {
+            if (item is OwnMessage)
+            {
+                return OwnerMessage;
+            }
+            else if(item is Message)
+            {
+                return AnotherMessage;
+            }
+
+            throw new System.NotImplementedException($"Uknown DataTemplate parameter. \nType: {nameof(MessageItemDataTemplateSelector)}\nParametr: {item}");
+        }
+    }
+}
