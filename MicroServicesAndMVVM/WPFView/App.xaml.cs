47a9c7baa493b6802c4b11a433f9a25704505be1
limeniye <limeniye@gamanet.com>
2022-05-23 04:52:02 +0300

    - Wpf changed constructor for MeetingService;

--- MicroServicesAndMVVM/WPFView/App.xaml.cs
+++ MicroServicesAndMVVM/WPFView/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using MeetingGrpcClient;
+﻿using Grpc.Net.Client;
+using GrpcCommon;
+using MeetingGrpcClient;
+using System.Net.Http;
 using System.Windows;
 
 namespace WPFView
@@ -8,9 +11,16 @@ namespace WPFView
         private void OnApplicationLaunched(object sender, StartupEventArgs e)
         {
             //IocService.Initialize();
+
+            var channel = GrpcChannel.ForAddress("https://localhost:7129/", new GrpcChannelOptions
+            {
+                HttpHandler = new SocketsHttpHandler()
+            });
+
+
             MainWindow = new MainWindow()
             {
-                DataContext = new MainViewModel(new MeetingService())
+                DataContext = new MainViewModel(new MeetingService(new Meeting.MeetingClient(channel)))
             };
             MainWindow.Show();
         }
