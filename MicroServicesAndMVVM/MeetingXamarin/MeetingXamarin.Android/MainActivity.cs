85f01e4ec3a0d89ac54fc2e8b5c89750e3360e1b
limeniye <limeniye@gamanet.com>
2022-05-24 06:51:37 +0300

    - Added camera renderer for android;

--- MicroServicesAndMVVM/MeetingXamarin/MeetingXamarin.Android/MainActivity.cs
+++ MicroServicesAndMVVM/MeetingXamarin/MeetingXamarin.Android/MainActivity.cs
@@ -4,14 +4,29 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Android;
+using Google.Android.Material.Snackbar;
+using Android.Views;
 
 namespace MeetingXamarin.Droid
 {
     [Activity(Label = "MeetingXamarin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public const int CameraPermissionsCode = 1;
+        public static readonly string[] CameraPermissions =
+        {
+            Manifest.Permission.Camera
+        };
+
+        public static event EventHandler CameraPermissionGranted;
+        private View _layout;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
@@ -23,6 +38,17 @@ namespace MeetingXamarin.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+
+            if (requestCode == CameraPermissionsCode && grantResults[0] == Permission.Denied)
+            {
+                Snackbar.Make(_layout, "Camera permission is denied. Please allow Camera use.", Snackbar.LengthIndefinite)
+                    .SetAction("OK", v => RequestPermissions(CameraPermissions, CameraPermissionsCode))
+                    .Show();
+                return;
+            }
+
+            CameraPermissionGranted?.Invoke(this, EventArgs.Empty);
         }
     }
 }
