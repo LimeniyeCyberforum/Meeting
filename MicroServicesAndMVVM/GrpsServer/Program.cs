717c7cb2f35cc31aa220408341e5174e51b6077b
limeniye <limeniye@gamanet.com>
2022-05-27 20:43:34 +0300

    - Nothing important;

--- MicroServicesAndMVVM/GrpsServer/Program.cs
+++ MicroServicesAndMVVM/GrpsServer/Program.cs
@@ -10,10 +10,10 @@ builder.Services.AddSingleton<IUsersCameraCaptureRepository, UsersCameraCaptureR
 builder.Services.AddSingleton<ChatService>();
 builder.Services.AddSingleton<UsersCameraCaptureService>();
 
-
 var app = builder.Build();
+
 app.UseGrpcWeb();
-app.MapGrpcService<MeetingService>().EnableGrpcWeb();
+app.MapGrpcService<MeetingService>().EnableGrpcWeb()/*.RequireHost("*:5010")*/;
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
