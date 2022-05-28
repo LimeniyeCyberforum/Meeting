f02dc7e53638d1f7ee7e9d2d8c253ddcf17bcebf
limeniye <limeniye@gamanet.com>
2022-05-29 02:06:46 +0300

    - StateTextBox. Added Status property;

--- MicroServicesAndMVVM/MeetingWPF.Controls/TextBox/TextBox.cs
+++ MicroServicesAndMVVM/MeetingWPF.Controls/TextBox/TextBox.cs
@@ -1,7 +1,61 @@
-﻿namespace MeetingWPF.Controls.TextBox
-{
-    public class TextBox : System.Windows.Controls.TextBox
-    {
+﻿using Common;
+using System;
+using System.Windows;
 
+namespace MeetingWPF.Controls.TextBox
+{
+    public enum Status
+    {
+        Empty,
+        Success, 
+        Failure,
+        Error
+    }
+
+    public class StateEmptiedEventArgs : EventArgs { }
+    public class StateSuccessedEventArgs : EventArgs { }
+
+    public class StateTextBox : System.Windows.Controls.TextBox
+    {
+        #region DependencyProperty : Placeholder
+        public static DependencyProperty PlaceholderProperty { get; } =
+            DependencyProperty.Register("Placeholder", typeof(string), 
+                typeof(StateTextBox), new FrameworkPropertyMetadata(string.Empty));
+
+        public string Placeholder
+        {
+            get => (string)GetValue(PlaceholderProperty);
+            set => SetValue(PlaceholderProperty, value);
+        }
+        #endregion
+
+        #region DependencyProperty : Status
+        public static DependencyProperty StatusProperty { get; } =
+            DependencyProperty.Register("Status", typeof(Status),
+                typeof(StateTextBox), new FrameworkPropertyMetadata(Status.Empty, delegate (DependencyObject s, DependencyPropertyChangedEventArgs e)
+                {
+                    (s as StateTextBox)?.OnStatusPropertyChanged(e);
+                }));
+
+        public Status Status
+        {
+            get => (Status)GetValue(StatusProperty);
+            set => SetValue(StatusProperty, value);
+        }
+        #endregion
+
+        public event TypedEventHandler<StateTextBox, StateEmptiedEventArgs>? Emptied;
+        public event TypedEventHandler<StateTextBox, StateSuccessedEventArgs>? Successed;
+
+        static StateTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(StateTextBox),
+             new FrameworkPropertyMetadata(typeof(StateTextBox)));
+        }
+
+        private void OnStatusPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
