0cd18ea1d991c89f65701e3ec74243f189e208ab
limeniye <limeniye@gmail.com>
2022-06-05 02:35:25 +0300

    - Removed usings;

--- src/Meeting.Business/Meeting.Business.Common/Abstractions/FrameCapture/FrameCaptureServiceAbstract - Events.cs
+++ src/Meeting.Business/Meeting.Business.Common/Abstractions/FrameCapture/FrameCaptureServiceAbstract - Events.cs
@@ -1,7 +1,5 @@
 ﻿using Meeting.Business.Common.EventArgs;
 using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace Meeting.Business.Common.Abstractions.FrameCapture
 {
