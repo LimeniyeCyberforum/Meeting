0b02e36ddf592008328a6a201843e34c772a1b54
limeniye <limeniye@gmail.com>
2022-06-06 01:08:08 +0300

    - CaptureFramesUserControl;

---
+++ src/Meeting.Wpf/CaptureFrames/CaptureFramesUserControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Meeting.Wpf.CaptureFrames
+{
+    public partial class CaptureFramesUserControl : UserControl
+    {
+        public CaptureFramesUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
