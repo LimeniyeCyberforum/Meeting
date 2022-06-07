0f7c184495edbd4976823a278e8b7b28632c3e4c
limeniye <limeniye@gmail.com>
2022-06-07 05:03:10 +0300

    - Added OwnerMessageDto;

---
+++ src/Meeting.Business/Meeting.Business.Common/DataTypes/OwnerMessageDto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Meeting.Business.Common.DataTypes
+{
+    public enum MessageStatus
+    {
+        Sending,
+        Unread,
+        Readed
+    }
+
+    public class OwnerMessageDto : MessageDto, IEquatable<OwnerMessageDto>
+    {
+        public MessageStatus Status { get; }
+
+        private readonly int hash;
+
+        public OwnerMessageDto(Guid guid, Guid userGuid, string message, string userName, DateTime? dateTime, MessageStatus status)
+            : base(guid, userGuid, message, userName, dateTime)
+        {
+            Status = status;
+
+            hash = (base.GetHashCode(), Status).GetHashCode();
+        }
+
+        public bool Equals(OwnerMessageDto other)
+        {
+            return base.Equals(other) && other.Status == Status;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is MessageDto currency && Equals(currency);
+        }
+
+        public override int GetHashCode() => hash;
+    }
+}
