da7c85c9d788f89809ef2b12da7efad2462972c9
limeniye <limeniye@gmail.com>
2022-06-07 11:59:40 +0300

    - Changed BaseInpc;

--- src/Toolkit.Xamarin/BaseInpc.cs
+++ src/Toolkit.Xamarin/BaseInpc.cs
@@ -1,45 +1,83 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace Toolkit.Xamarin
 {
-    public abstract class BaseInpc : INotifyPropertyChanged
+    public delegate bool Equality<T>(T left, T right);
+
+    public abstract partial class BaseInpc : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
-            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-        public void OnPropertyChanged(IEnumerable<string> propList)
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
         {
-            foreach (string propertyName in propList.Where(name => !string.IsNullOrWhiteSpace(name)))
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        public void OnPropertyChanged(params string[] propList)
+        protected bool Set<T>(ref T propertyField, T newValue, Equality<T> equality = null, [CallerMemberName] string propertyName = null)
         {
-            foreach (string propertyName in propList.Where(name => !string.IsNullOrWhiteSpace(name)))
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            bool isEquals = propertyField == null || newValue == null
+                ? propertyField == null && newValue == null
+                : ReferenceEquals(propertyField, newValue) ||
+                    (
+                        equality != null
+                        ? equality(propertyField, newValue)
+                        : propertyField is IEquatable<T> equatable
+                            ? equatable.Equals(newValue)
+                            : propertyField.Equals(newValue)
+                    );
+            if (!isEquals)
+            {
+                T oldValue = propertyField;
+                propertyField = newValue;
+                RaisePropertyChanged(propertyName);
+
+                ProtectedPropertyChanged?.Invoke(propertyName, oldValue, newValue);
+            }
+
+            return !isEquals;
         }
 
-        public void OnAllPropertyChanged()
-            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+        protected static event PropertyChangedHandler ProtectedPropertyChanged;
+    }
 
-        public void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
-             => PropertyChanged?.Invoke(this, e);
+    public abstract partial class BaseInpc : INotifyPropertyChanged
+    {
+        public static event EventHandler<PropertyChangedEventArgs> StaticPropertyChanged;
 
-        protected virtual void Set<T>(ref T fieldProperty, T newValue, [CallerMemberName] string propertyName = "")
+        protected static void RaiseStaticPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            if ((fieldProperty != null && !fieldProperty.Equals(newValue)) || (fieldProperty == null && newValue != null))
-                PropertyNewValue(ref fieldProperty, newValue, propertyName);
+            StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(propertyName));
         }
 
-        protected virtual void PropertyNewValue<T>(ref T fieldProperty, T newValue, string propertyName)
+        protected static bool StaticSet<T>(ref T propertyField, T newValue, Equality<T> equality = null, [CallerMemberName] string propertyName = null)
         {
-            fieldProperty = newValue;
-            OnPropertyChanged(propertyName);
+            bool isEquals = propertyField == null || newValue == null
+                ? propertyField == null && newValue == null
+                : ReferenceEquals(propertyField, newValue) ||
+                    (
+                        equality != null
+                        ? equality(propertyField, newValue)
+                        : propertyField is IEquatable<T> equatable
+                            ? equatable.Equals(newValue)
+                            : propertyField.Equals(newValue)
+                    );
+
+            if (!isEquals)
+            {
+                T oldValue = propertyField;
+                propertyField = newValue;
+                RaiseStaticPropertyChanged(propertyName);
+
+                ProtectedStaticPropertyChanged?.Invoke(propertyName, oldValue, newValue);
+            }
+
+            return !isEquals;
         }
+
+        protected static event PropertyChangedHandler ProtectedStaticPropertyChanged;
+
+        protected delegate void PropertyChangedHandler(string propertyName, object oldValue, object newValue);
     }
 }
