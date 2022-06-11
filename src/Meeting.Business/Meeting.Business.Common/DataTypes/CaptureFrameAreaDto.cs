a06d49d1b13b16498142936d979ae6f744004d5a
limeniye <limeniye@gmail.com>
2022-06-11 07:45:29 +0300

    - Added is Active to CaptureFrameAreaDto;

--- src/Meeting.Business/Meeting.Business.Common/DataTypes/CaptureFrameAreaDto.cs
+++ src/Meeting.Business/Meeting.Business.Common/DataTypes/CaptureFrameAreaDto.cs
@@ -6,17 +6,20 @@ namespace Meeting.Business.Common.DataTypes
     public class CaptureFrameAreaDto : GuidDto
     {
         public Guid OwnerGuid { get; }
+        public bool IsActive { get; }
 
-        public CaptureFrameAreaDto(Guid areaGuid, Guid ownerGuid)
+        public CaptureFrameAreaDto(Guid areaGuid, Guid ownerGuid, bool isActive)
             : base(areaGuid)
         {
             OwnerGuid = ownerGuid;
+            IsActive = isActive;
         }
 
         protected override bool EqualsCore(GuidDto dto)
         {
             return dto is CaptureFrameAreaDto other &&
-                Equals(other.OwnerGuid, OwnerGuid);
+                OwnerGuid == other.OwnerGuid &&
+                other.IsActive == IsActive;
         }
     }
 }
