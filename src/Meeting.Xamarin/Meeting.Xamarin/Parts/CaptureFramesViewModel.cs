40fc77e9c35b160cf35f0e36ecaba1d24966d66d
limeniye <limeniye@gmail.com>
2022-06-12 05:53:01 +0300

    - [Xamarin] CaptureFrameViewModel memoty optimizing;

--- src/Meeting.Xamarin/Meeting.Xamarin/Parts/CaptureFramesViewModel.cs
+++ src/Meeting.Xamarin/Meeting.Xamarin/Parts/CaptureFramesViewModel.cs
@@ -2,37 +2,37 @@
 using Meeting.Business.Common.Abstractions.FrameCapture;
 using Meeting.Business.Common.DataTypes;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Reactive.Disposables;
 using Toolkit.Xamarin;
+using Xamarin.Forms;
 
 namespace Meeting.Xamarin.Parts
 {
     public class CaptureFrameViewModel : BaseInpc
     {
-        private Guid _ownerGuid, _areaGuid;
+        private Guid _areaGuid, _ownerGuid;
         private string _ownerName;
         private byte[] _data;
 
-        public Guid OwnerGuid { get => _ownerGuid; set => Set(ref _ownerGuid, value); }
         public Guid AreaGuid { get => _areaGuid; set => Set(ref _areaGuid, value); }
+        public Guid OwnerGuid { get => _ownerGuid; set => Set(ref _ownerGuid, value); }
         public string OwnerName { get => _ownerName; set => Set(ref _ownerName, value); }
         public byte[] Data { get => _data; set => Set(ref _data, value); }
 
-        public CaptureFrameViewModel(Guid ownerGuid, string ownerName, Guid areaGuid, byte[] data)
+        public CaptureFrameViewModel(Guid areaGuid, Guid ownerGuid, string ownerName, byte[] data)
         {
-            OwnerGuid = ownerGuid;
             AreaGuid = areaGuid;
-            Data = data;
+            OwnerGuid = ownerGuid;
             OwnerName = ownerName;
+            Data = data;
         }
     }
 
     public class CaptureFramesViewModel : BaseInpc
     {
+        private readonly SerialDisposable eventSubscriptions = new SerialDisposable();
         private readonly CaptureFramesServiceAbstract _captureFramesService;
-        //private readonly CamStreaming _cam;
         private UserDto _currentUser;
 
         private readonly IMeetingAuthorization _authorizationService;
@@ -45,34 +45,56 @@ namespace Meeting.Xamarin.Parts
 
         public CaptureFramesViewModel(CaptureFramesServiceAbstract captureFramesService, IMeetingUsers users, IMeetingAuthorization authorizationService)
         {
-            //_cam = CamInitializeTest();
-            //_cam.CaptureFrameChanged += OnOwnCaptureFrameChanged;
-
-
             _authorizationService = authorizationService;
-            _authorizationService.AuthorizationStateChanged += OnConnectionStateChanged;
-
             _captureFramesService = captureFramesService;
-            _captureFramesService.CaptureFrameStateChanged += OnCaptureFrameStateChanged;
-            _captureFramesService.CaptureFrameChanged += OnCaptureFrameChanged;
-
-            foreach (var item in _captureFramesService.ActiveCaptureFrames)
-            {
-                CaptureFrameAreas.Add(item.AreaGuid, new CaptureFrameViewModel(item.OwnerGuid, String.Empty, item.AreaGuid, null));
-            }
-
             _meetingUsers = users;
-            _meetingUsers.Users.UsersChanged += OnUsersChanged;
+
+            Initizalize();
+            Subscriptions();
+        }
+
+        private void Initizalize()
+        {
+            foreach (var item in _captureFramesService.ActiveCaptureFrames)
+                CaptureFrameAreas.Add(item.Key, new CaptureFrameViewModel(item.Key, item.Value.OwnerGuid, String.Empty, null));
 
             foreach (var item in _meetingUsers.Users.Users)
             {
                 var captureFrameArea = CaptureFrameAreas.Values.FirstOrDefault(x => x.OwnerGuid == item.Key || x.AreaGuid == item.Key);
 
                 if (captureFrameArea == null)
-                    CaptureFrameAreas.Add(item.Key, new CaptureFrameViewModel(item.Key, item.Value.UserName, item.Key, null));
+                    CaptureFrameAreas.Add(item.Key, new CaptureFrameViewModel(item.Key, item.Value.Guid, item.Value.UserName, null));
             }
+        }
 
+        private void Subscriptions()
+        {
+            eventSubscriptions.Disposable = null;
+            CompositeDisposable disposable = new CompositeDisposable();
+
+            _meetingUsers.Users.UsersChanged += OnUsersChanged;
+            _authorizationService.AuthorizationStateChanged += OnConnectionStateChanged;
+            _captureFramesService.CaptureFrameStateChanged += OnCaptureFrameStateChanged;
             ProtectedPropertyChanged += OnProtectedPropertyChanged;
+
+            disposable.Add(Disposable.Create(delegate
+            {
+                _meetingUsers.Users.UsersChanged -= OnUsersChanged;
+                _authorizationService.AuthorizationStateChanged -= OnConnectionStateChanged;
+                _captureFramesService.CaptureFrameStateChanged -= OnCaptureFrameStateChanged;
+                ProtectedPropertyChanged -= OnProtectedPropertyChanged;
+            }));
+            eventSubscriptions.Disposable = disposable;
+        }
+
+        protected override void OnAppearing()
+        {
+            _captureFramesService.CaptureFrameChanged += OnCaptureFrameChanged;
+        }
+
+        protected override void OnDisappearing()
+        {
+            _captureFramesService.CaptureFrameChanged -= OnCaptureFrameChanged;
         }
 
 
@@ -84,29 +106,17 @@ namespace Meeting.Xamarin.Parts
             }
             else if (action == UserConnectionState.Disconnected)
             {
+                _currentUser = null;
+
                 //_ = _cam.Stop();
                 //_cam.Dispose();
             }
         }
 
-        //private CamStreaming? CamInitializeTest()
-        //{
-        //    CamStreaming result = null;
-        //    var selectedCameraDeviceId = CameraDevicesEnumerator.GetAllConnectedCameras()[0].OpenCvId;
-        //    if (_cam == null || _cam.CameraDeviceId != selectedCameraDeviceId)
-        //    {
-        //        _cam?.Dispose();
-        //        result = new CamStreaming(frameWidth: 300, frameHeight: 300, selectedCameraDeviceId);
-        //    }
-
-        //    return result;
-        //}
-
         private void OnOwnCaptureFrameChanged(object sender, byte[] e)
         {
             _captureFramesService.SendFrameAsync(e, _currentUser.Guid, DateTime.UtcNow);
         }
-
 
         private void OnUsersChanged(object sender, Framework.EventArgs.NotifyDictionaryChangedEventArgs<Guid, Business.Common.DataTypes.UserDto> e)
         {
@@ -114,25 +124,29 @@ namespace Meeting.Xamarin.Parts
             var captureFrameArea = CaptureFrameAreas.Values.FirstOrDefault(x => x.OwnerGuid == item.Guid || x.AreaGuid == item.Guid);
 
             if (captureFrameArea == null)
-            {
-                CaptureFrameAreas.Add(item.Guid, new CaptureFrameViewModel(item.Guid, item.UserName, item.Guid, null));
-            }
+                CaptureFrameAreas.Add(item.Guid, new CaptureFrameViewModel(item.Guid, item.Guid, item.UserName, null));
         }
 
         private void OnCaptureFrameStateChanged(object sender, Business.Common.EventArgs.CaptureFrameStateEventArgs e)
         {
-            if (e.IsOn)
+            switch (e.Action)
             {
-                CaptureFrameAreas.Add(e.CaptureAreadGuid, new CaptureFrameViewModel(e.OwnerGuid, null, e.CaptureAreadGuid, null));
-            }
-            else
-            {
-                CaptureFrameAreas.Remove(e.CaptureAreadGuid);
+                case Business.Common.EventArgs.CaptureFrameState.Disabled:
+                    var captureFrame = CaptureFrameAreas[e.CaptureAreadGuid];
+                    captureFrame.Data = null;
+                    break;
+                case Business.Common.EventArgs.CaptureFrameState.Created:
+                    CaptureFrameAreas.Add(e.CaptureAreadGuid, new CaptureFrameViewModel(e.OwnerGuid, e.CaptureAreadGuid, "steve", null));
+                    break;
+                case Business.Common.EventArgs.CaptureFrameState.Removed:
+                    CaptureFrameAreas.Remove(e.CaptureAreadGuid);
+                    break;
             }
         }
 
         private void OnCaptureFrameChanged(object sender, Business.Common.EventArgs.CaptureFrameEventArgs e)
         {
+            //Device.
             var area = CaptureFrameAreas[e.CaptureAreadGuid];
             area.Data = e.Bytes;
         }
