cd4a982505063456d34f65b851007695ab7fff8f
limeniye <limeniye@gmail.com>
2022-06-12 13:39:05 +0300

    - [WPF] CaptureFrames little changes;

---
+++ src/Meeting.Xamarin/Meeting.Xamarin/Parts/CaptureFramesView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Meeting.Xamarin.Parts
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CaptureFramesView : Grid
+    {
+        public CaptureFramesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
