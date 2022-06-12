ac15ae3486e4abf029e399f4338b1fcad222e2b3
limeniye <limeniye@gmail.com>
2022-06-12 05:05:17 +0300

    - [MainViewModel] Fixed memory leak;

--- src/Meeting.Wpf/Windows/MainViewModel.cs
+++ src/Meeting.Wpf/Windows/MainViewModel.cs
@@ -3,11 +3,15 @@ using Meeting.Wpf.Chat;
 using Meeting.Wpf.Connect;
 using MvvmCommon.WindowsDesktop;
 using Meeting.Wpf.CaptureFrames;
+using System.Reactive.Disposables;
+using System;
 
 namespace Meeting.Wpf.Windows
 {
-    public class MainViewModel : BaseInpc
+    public class MeetingViewModel : BaseInpc
     {
+        private readonly SerialDisposable eventSubscriptions = new SerialDisposable();
+
         private readonly IMeetingService _meetingService;
 
         private bool _isConnected = false;
@@ -17,17 +21,27 @@ namespace Meeting.Wpf.Windows
         public ConnectViewModel ConnectVM { get; }
         public CaptureFramesViewModel CaptureFramesVM { get; }
 
-        public MainViewModel(IMeetingService meetingService)
+        public MeetingViewModel(IMeetingService meetingService)
         {
             _meetingService = meetingService;
-            _meetingService.Chat.ChatSubscribeAsync();
-            _meetingService.Users.UsersSubscribeAsync();
-            _meetingService.CaptureFrames.CaptureFrameAreasSubscribeAsync();
-            _meetingService.CaptureFrames.CaptureFramesSubscribeAsync();
+
             ChatVM = new ChatViewModel(_meetingService.Chat, _meetingService);
             ConnectVM = new ConnectViewModel(_meetingService);
             CaptureFramesVM = new CaptureFramesViewModel(_meetingService.CaptureFrames, _meetingService, _meetingService);
+
+            Subscriptions();
+        }
+
+        private void Subscriptions()
+        {
+            eventSubscriptions.Disposable = null;
+            CompositeDisposable disposable = new CompositeDisposable();
             _meetingService.AuthorizationStateChanged += OnConnectionStateChanged;
+            disposable.Add(Disposable.Create(delegate
+            {
+                _meetingService.AuthorizationStateChanged -= OnConnectionStateChanged;
+            }));
+            eventSubscriptions.Disposable = disposable;
         }
 
         private void OnConnectionStateChanged(object? sender, UserConnectionState action)
