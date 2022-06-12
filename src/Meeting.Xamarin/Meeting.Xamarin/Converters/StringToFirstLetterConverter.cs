5b398ef7deb9bd95effb29a0feca440e78f46ddd
limeniye <limeniye@gmail.com>
2022-06-12 15:39:15 +0300

    - Added converters;

---
+++ src/Meeting.Xamarin/Meeting.Xamarin/Converters/StringToFirstLetterConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Meeting.Xamarin.Converters
+{
+    public class StringToFirstLetterConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is string text))
+                return "user";
+
+            if (string.IsNullOrWhiteSpace(text))
+                return "user";
+
+            return char.ToUpper(text[0]);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
