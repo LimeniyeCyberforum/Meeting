53603a06e626cb595204614a576f799f05cd019c
limeniye <limeniye@gmail.com>
2022-06-12 05:51:56 +0300

    - [WPF] lifecycle server subscription;

--- src/Meeting.Wpf/App.xaml.cs
+++ src/Meeting.Wpf/App.xaml.cs
@@ -1,18 +1,38 @@
-﻿using Meeting.Business.GrpcClient;
+﻿using Meeting.Business.Common.Abstractions;
+using Meeting.Business.GrpcClient;
 using Meeting.Wpf.Windows;
+using System;
 using System.Windows;
 
 namespace Meeting.Wpf
 {
     public partial class App : Application
     {
+        private IMeetingService _meetingService;
+
         private void OnApplicationLaunched(object sender, StartupEventArgs e)
         {
-            MainWindow = new MainWindow()
+            _meetingService = new MeetingService();
+            _meetingService.Chat.ChatSubscribeAsync();
+            _meetingService.Users.UsersSubscribeAsync();
+            _meetingService.CaptureFrames.CaptureFrameAreasSubscribeAsync();
+            _meetingService.CaptureFrames.CaptureFramesSubscribeAsync();
+
+            MainWindow = new MeetingWindow()
             {
-                DataContext = new MainViewModel(new MeetingService())
+                DataContext = new MeetingViewModel(_meetingService)
             };
             MainWindow.Show();
+        }
+
+        protected override void OnSessionEnding(SessionEndingCancelEventArgs e)
+        {
+            _meetingService.Chat.ChatUnsubscribe();
+            _meetingService.Users.UsersUnsubscribe();
+            _meetingService.CaptureFrames.CaptureFramesUnsubscribe();
+            _meetingService.CaptureFrames.CaptureFramesUnsubscribe();
+
+            base.OnSessionEnding(e);
         }
     }
 }
