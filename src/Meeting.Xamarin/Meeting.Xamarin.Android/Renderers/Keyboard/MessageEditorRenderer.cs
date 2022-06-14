7ac79575cf10189b6634ed9004d47e80b42c3b89
limeniye <limeniye@gmail.com>
2022-06-14 08:11:28 +0300

    - Added MessageEditorRenderer;

---
+++ src/Meeting.Xamarin/Meeting.Xamarin.Android/Renderers/Keyboard/MessageEditorRenderer.cs
@@ -0,0 +1,27 @@
+﻿using Android.Content;
+using Meeting.Xamarin.Droid.Renderers.Keyboard;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Editor), typeof(MessageEditorRenderer))]
+namespace Meeting.Xamarin.Droid.Renderers.Keyboard
+{ 
+    public class MessageEditorRenderer : EditorRenderer
+    {
+        public MessageEditorRenderer(Context context)
+            : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.ShowSoftInputOnFocus = false;
+            }
+        }
+    }
+}
