09e86f8444db756181807d3e7a7509579b7f1847
limeniye <limeniye@gmail.com>
2022-07-10 01:10:23 +0300

    - Prism IoC;

--- src/Meeting.Wpf/App.xaml.cs
+++ src/Meeting.Wpf/App.xaml.cs
@@ -1,38 +1,46 @@
 ﻿using Meeting.Core.Common;
-using Meeting.Core.GrpcClient;
-using Meeting.Wpf.ViewModels;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Prism.Regions;
 using System.Windows;
 
 namespace Meeting.Wpf
 {
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
         private IMeetingService? _meetingService;
-        private MeetingViewModel? _meetingViewModel;
-
-        private void OnApplicationLaunched(object sender, StartupEventArgs e)
-        {
-            _meetingService = MeetingService.Instance;
-            _meetingService.Chat.ChatSubscribeAsync();
-            _meetingService.Users.UsersSubscribeAsync();
-            _meetingService.CaptureFrames.CaptureFrameAreasSubscribeAsync();
-            _meetingService.CaptureFrames.CaptureFramesSubscribeAsync();
-
-            _meetingViewModel = new MeetingViewModel(_meetingService);
-
-            MainWindow = new MeetingWindow()
-            {
-                DataContext = _meetingViewModel
-            };
-
-            MainWindow.Show();
-        }
 
         protected override void OnExit(ExitEventArgs e)
         {
             _meetingService?.Dispose();
-            _meetingViewModel?.Dispose();
             base.OnExit(e);
+        }
+
+        public void OnDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            _meetingService?.Dispose();
+            MessageBox.Show("Unhandled exception occurred: \n" + e.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MeetingWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.ConfigurationServices();
+        }
+
+        protected override void OnInitialized()
+        {
+            _meetingService = Container.Resolve<IMeetingService>();
+            _meetingService.Chat.ChatSubscribeAsync();
+            _meetingService.Users.UsersSubscribeAsync();
+            _meetingService.CaptureFrames.CaptureFrameAreasSubscribeAsync();
+            _meetingService.CaptureFrames.CaptureFramesSubscribeAsync();
+            (App.Current.MainWindow.DataContext as INavigationAware)?.OnNavigatedTo(null);
+            base.OnInitialized();
         }
     }
 }
---
+++ src/Meeting.Wpf/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Meeting.Core.Common;
+using Meeting.Core.GrpcClient;
+using Meeting.Wpf.ViewModels;
+using Prism.Ioc;
+
+namespace Meeting.Wpf
+{
+    public static class Startup
+    {
+        public static void ConfigurationServices(this IContainerRegistry services)
+        {
+            services.RegisterSingleton<IMeetingService>(() => MeetingService.Instance);
+            services.Add<MeetingWindow, MeetingViewModel>();
+        }
+
+        private static void Add<TView, TViewModel>(this IContainerRegistry containerRegistry, string? name = null)
+        {
+            containerRegistry.RegisterForNavigation<TView, TViewModel>(name);
+        }
+    }
+}
