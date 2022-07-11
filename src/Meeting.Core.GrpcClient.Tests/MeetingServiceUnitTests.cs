55ff58b7910b8c72ed9a9f3b7cffee74a870876c
limeniye <limeniye@gmail.com>
2022-07-11 12:33:58 +0300

    - Connection_Test;

---
+++ src/Meeting.Core.GrpcClient.Tests/MeetingServiceUnitTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Core.Testing;
+using MeetingProtobuf.Protos;
+using Moq;
+using Xunit;
+using static MeetingProtobuf.Protos.Authorization;
+
+namespace Meeting.Core.GrpcClient.Tests
+{
+    public sealed class MeetingServiceUnitTests
+    {
+        [Fact]
+        public void Connection_Test()
+        {
+            // Arrange
+            var mockClient = new Mock<AuthorizationClient>();
+
+
+            // Use a factory method provided by Grpc.Core.Testing.TestCalls to create an instance of a call.
+            var fakeCall = TestCalls.AsyncUnaryCall(Task.FromResult(new ConnectResponse()),
+                Task.FromResult(new Metadata()), () => Status.DefaultSuccess, () => new Metadata(), () => { });
+
+            mockClient.Setup(m => m.ConnectAsync(It.IsAny<ConnectRequest>(), null, null, CancellationToken.None))
+                      .Returns(fakeCall);
+
+
+            var test = mockClient.Object.ConnectAsync(new ConnectRequest() { Username = "limpopo" });
+
+            //// Assert
+            Assert.Same(fakeCall, test);
+
+            //var worker = new Worker(mockClient.Object, mockRepository.Object);
+
+            //// Act
+            //await worker.StartAsync(CancellationToken.None);
+
+            //// Assert
+            //mockRepository.Verify(v => v.SaveGreeting("Test"));
+        }
+    }
+}
