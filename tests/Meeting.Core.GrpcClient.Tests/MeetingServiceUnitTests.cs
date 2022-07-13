edc0bd8a1cf6b3f9fd39aa5f39777863c5c6b717
limeniye <limeniye@gmail.com>
2022-07-13 12:09:09 +0300

    - Added CallHelpers;

--- tests/Meeting.Core.GrpcClient.Tests/MeetingServiceUnitTests.cs
+++ tests/Meeting.Core.GrpcClient.Tests/MeetingServiceUnitTests.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Grpc.Core;
 using Grpc.Core.Testing;
+using Meeting.Core.GrpcClientTests.App_Infrastructure.Extensions;
 using MeetingProtobuf.Protos;
 using Moq;
 using Xunit;
@@ -15,29 +16,16 @@ namespace Meeting.Core.GrpcClient.Tests
         public void Connection_Test()
         {
             // Arrange
+            var mockCall = CallHelpers.CreateAsyncUnaryCall(new ConnectResponse() { IsSuccess = true });
             var mockClient = new Mock<AuthorizationClient>();
-
-
-            // Use a factory method provided by Grpc.Core.Testing.TestCalls to create an instance of a call.
-            var fakeCall = TestCalls.AsyncUnaryCall(Task.FromResult(new ConnectResponse()),
-                Task.FromResult(new Metadata()), () => Status.DefaultSuccess, () => new Metadata(), () => { });
-
             mockClient.Setup(m => m.ConnectAsync(It.IsAny<ConnectRequest>(), null, null, CancellationToken.None))
-                      .Returns(fakeCall);
+                      .Returns(mockCall);
 
-
+            // Act
             var test = mockClient.Object.ConnectAsync(new ConnectRequest() { Username = "limpopo" });
 
-            //// Assert
-            Assert.Same(fakeCall, test);
-
-            //var worker = new Worker(mockClient.Object, mockRepository.Object);
-
-            //// Act
-            //await worker.StartAsync(CancellationToken.None);
-
-            //// Assert
-            //mockRepository.Verify(v => v.SaveGreeting("Test"));
+            // Assert
+            Assert.Same(mockCall, test);
         }
     }
 }
