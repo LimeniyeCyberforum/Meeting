3354824d5845bfc3aa6aabf256d50f6108481afb
limeniye <limeniye@gmail.com>
2022-07-13 14:37:33 +0300

    - Created Meeting.Tests.ViewModels;

---
+++ tests/Meeting.Tests.ViewModels/MeetingViewModelTests.cs
@@ -0,0 +1,15 @@
+﻿using Meeting.Wpf.ViewModels;
+using Moq;
+using Xunit;
+
+namespace Meeting.Tests.ViewModels
+{
+    public class MeetingViewModelTests
+    { 
+        [Fact]
+        public void MainViewModelConstructor()
+        {
+
+        }
+    }
+}
