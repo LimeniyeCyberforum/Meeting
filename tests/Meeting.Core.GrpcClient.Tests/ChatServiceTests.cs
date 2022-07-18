114ceb1929504206f514b24d16ac449787765465
limeniye <limeniye@gmail.com>
2022-07-18 22:21:59 +0300

    - ChatSubscribeAsyncTest and UsersSubscribeAsyncTest;

---
+++ tests/Meeting.Core.GrpcClient.Tests/ChatServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Meeting.Core.GrpcClient.Tests.Models;
+using Meeting.Core.GrpcClient.Tests.Utils;
+using MeetingProtobuf.Protos;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using static MeetingProtobuf.Protos.Chat;
+
+namespace Meeting.Core.GrpcClient.Tests
+{
+    public class ChatServiceTests
+    {
+        [Fact]
+        public async Task ChatSubscribeAsyncTest()
+        {
+            //Arrange
+            var chatMockClient = new Mock<ChatClient>();
+
+            List<LobbyMessageResponse> lobbyMessagesResponse =
+                new List<LobbyMessageResponse>
+                {
+                    new LobbyMessageResponse
+                    {
+                        Action = MeetingProtobuf.Protos.Action.Added,
+                        LobbyMessage = new LobbyMessage
+                        {
+                            Message = "HelloWorld!",
+                            MessageGuid = Guid.NewGuid().ToString(),
+                            Time = DateTime.UtcNow.ToTimestamp(),
+                            UserGuid = Guid.NewGuid().ToString(),
+                            Username = "Elliot"
+                        }
+                    }
+                };
+
+            var reader = new AsyncStreamReader<LobbyMessageResponse>(lobbyMessagesResponse);
+
+            var fakeCall = CallHelpers.CreateAsyncServerStreamingCall(reader);
+
+
+            chatMockClient.Setup(m => m.MessagesSubscribe(new Empty(), null, null, CancellationToken.None))
+                .Returns(fakeCall);
+
+            // Act
+            ChatService service = new ChatService(chatMockClient.Object);
+
+            await service.ChatSubscribeAsync();
+
+            // Assert
+            Assert.True(service.Messages.Count > 0);
+
+            service.Dispose();
+        }
+    }
+}
---
+++ tests/Meeting.Core.GrpcClient.Tests/UsersServiceTests.cs
@@ -0,0 +1,51 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Core.Testing;
+using Meeting.Core.GrpcClient.Tests.Models;
+using Meeting.Core.GrpcClient.Tests.Utils;
+using MeetingProtobuf.Protos;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using UsersClient = MeetingProtobuf.Protos.Users.UsersClient;
+
+
+namespace Meeting.Core.GrpcClient.Tests
+{
+    public class UsersServiceTests
+    {
+        [Fact]
+        public async Task UsersSubscribeAsyncTest()
+        {
+            //Arrange
+            var usersMockClient = new Mock<UsersClient>();
+
+            List<UserOnlineStatusResponse> userOnlineStatusResponses = 
+                new List<UserOnlineStatusResponse>
+                {
+                    new UserOnlineStatusResponse { User = new User { Name = "Elliot", UserGuid = Guid.NewGuid().ToString() }, Action = MeetingProtobuf.Protos.Action.Added },
+                    new UserOnlineStatusResponse { User = new User { Name = "Ollie", UserGuid = Guid.NewGuid().ToString() }, Action = MeetingProtobuf.Protos.Action.Added }
+                };
+
+            var reader = new AsyncStreamReader<UserOnlineStatusResponse>(userOnlineStatusResponses);
+
+            var fakeCall = CallHelpers.CreateAsyncServerStreamingCall(reader);
+
+            usersMockClient.Setup(m => m.UsersSubscribe(new Empty(), null, null, CancellationToken.None))
+                .Returns(fakeCall);
+
+            // Act
+            UsersService service = new UsersService(usersMockClient.Object);
+            
+            await service.UsersSubscribeAsync();
+
+            // Assert
+            Assert.True(service.Users.Count > 0);
+
+            service.Dispose();
+        }
+    }
+}
