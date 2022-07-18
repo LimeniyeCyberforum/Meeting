238f3d2a3c36828ce33d13b1afda36def4b62f6c
limeniye <limeniye@gmail.com>
2022-07-18 16:11:44 +0300

    - Removed App_Infrastucure;

---
+++ tests/Meeting.Tests.ViewModels/ConnectViewModelTests.cs
@@ -0,0 +1,26 @@
+﻿using Meeting.Core.Common;
+using Meeting.Wpf.ViewModels;
+using Moq;
+using Xunit;
+
+namespace Meeting.Tests.ViewModels
+{
+    public class ConnectViewModelTests
+    {
+        //[Fact]
+        //public void JoinCommand()
+        //{
+        //    //Arrange
+        //    var mockWindowService = new Mock<IMeetingAuthorization>();
+        //    mockWindowService.Setup(w => w.ShowWindow<AboutWindow>()).Verifiable();
+        //    var mockApplicationService = new Mock<IApplicationService>();
+
+        //    //Act
+        //    var mainViewModel = new ConnectViewModel();
+        //    mainViewModel.ShowAboutCommand.Execute(null);
+
+        //    //Assert
+        //    mockWindowService.Verify(w => w.ShowWindow<AboutWindow>(), Times.AtLeastOnce);
+        //}
+    }
+}
---
+++ tests/Meeting.Tests/MeetingServiceIntegrationTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using Moq;
+using Xunit;
+using static MeetingProtobuf.Protos.Authorization;
+
+namespace Meeting.Tests
+{
+    //public sealed class MeetingServiceIntegrationTests : IClassFixture<CustomWebApplicationFactory<>>
+    //{
+    //    [Fact]
+    //    public void Connection_Test()
+    //    {
+    //         Arrange
+    //        var mockCall = CallHelpers.CreateAsyncUnaryCall(new ConnectResponse() { IsSuccess = true });
+    //        var mockClient = new Mock<AuthorizationClient>();
+    //        mockClient.Setup(m => m.ConnectAsync(It.IsAny<ConnectRequest>(), null, null, CancellationToken.None))
+    //                  .Returns(mockCall);
+
+    //         Act
+    //        var test = mockClient.Object.ConnectAsync(new ConnectRequest() { Username = "limpopo" });
+
+    //         Assert
+    //        Assert.Same(mockCall, test);
+    //    }
+    //}
+}
