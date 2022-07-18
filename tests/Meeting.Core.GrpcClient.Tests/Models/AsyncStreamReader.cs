0ce77ee7b354eef11bcd998b1c1c23a67c5e0591
limeniye <limeniye@gmail.com>
2022-07-18 22:21:34 +0300

    - AsyncSteamReader and CallHalpers;

---
+++ tests/Meeting.Core.GrpcClient.Tests/Models/AsyncStreamReader.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Meeting.Core.GrpcClient.Tests.Models
+{
+    internal class AsyncStreamReader<T> : IAsyncStreamReader<T>
+    {
+        private readonly IEnumerator<T> enumerator;
+
+        public AsyncStreamReader(IEnumerable<T> results)
+        {
+            enumerator = results.GetEnumerator();
+        }
+
+        public T Current => enumerator.Current;
+
+        public Task<bool> MoveNext(CancellationToken cancellationToken) =>
+            Task.Run(() => enumerator.MoveNext());
+    }
+}
